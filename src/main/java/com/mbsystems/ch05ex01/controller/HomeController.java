4be1a82f674a90449fb536c1d2dc24e1bbb27502
basilk27 <basilkiwanuka@icloud.com>
2020-05-22 13:33:56 -0500

    added code for different ways to get the security context

--- src/main/java/com/mbsystems/ch05ex01/controller/HomeController.java
+++ src/main/java/com/mbsystems/ch05ex01/controller/HomeController.java
@@ -1,13 +1,67 @@
 package com.mbsystems.ch05ex01.controller;
 
+import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
+import org.springframework.security.concurrent.DelegatingSecurityContextExecutorService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.springframework.security.core.context.SecurityContextHolder.getContext;
 
 @RestController
 public class HomeController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Basil hello security example";
+        return "Basil hello security example off";
     }
+
+    @GetMapping("/hello/context")
+    public String hellocontexts() {
+        SecurityContext securityContext = getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        return "Hello security example : " + authentication.getName();
+    }
+    @GetMapping("/auth")
+    public String helloauth( Authentication auth) {
+        return "Hello security example : " + auth.getName();
+    }
+
+    @GetMapping("/ciao")
+    public String ciao() throws Exception {
+        Callable<String> task = () -> {
+            SecurityContext context = SecurityContextHolder.getContext();
+            return context.getAuthentication().getName();
+        };
+
+        ExecutorService e = Executors.newCachedThreadPool();
+        try {
+            var contextTask = new DelegatingSecurityContextCallable<>(task);
+            return "Ciao, " + e.submit(contextTask).get() + "!";
+        } finally {
+            e.shutdown();
+        }
+    }
+    @GetMapping("/hola")
+    public String hola() throws Exception {
+        Callable<String> task = () -> {
+            SecurityContext context = SecurityContextHolder.getContext();
+            return context.getAuthentication().getName();
+        };
+
+        ExecutorService e = Executors.newCachedThreadPool();
+        e = new DelegatingSecurityContextExecutorService(e);
+        try {
+            return "Hola, " + e.submit(task).get() + "!";
+        } finally {
+            e.shutdown();
+        }
+    }
+
 }
